8627c389c674dd1bbe898bb5bfae99497bb7373c
Brendan-funk <brendanzfunk@gmail.com>
2022-03-12 19:14:16 +0000

    added the user seed

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone, email, is_owner)
+  VALUES ('Alice', '604-554-2213', 'example@gmail.com', false),
+  ('Jeremy', '603-443-8473', 'test@hotmail.com', false),
+  ('Bob', '604-215-5529', 'gungaginga@gmail.com', false),
+  ('Kennedy', '604-217-9518', 'best@gmail.com', false);
