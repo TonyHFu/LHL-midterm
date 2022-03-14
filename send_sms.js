d32093848e2188585ecbca2cc3b783f32fa68fa8
Your Name <your@email.com>
2022-03-14 18:08:49 -0400

    commit before pulling changes on master

--- send_sms.js
+++ send_sms.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
-// console.log(process.env);// remove this after you've confirmed it working
 const twilio = require('twilio');
 
-// Download the helper library from https://www.twilio.com/docs/node/install
-// Find your Account SID and Auth Token at twilio.com/console
-// and set the environment variables. See http://twil.io/secure
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = new twilio(accountSid, authToken);
 
 client.messages
   .create({
     body: 'Your order is ready for pickup!',
-    from: '+16137093672',// Text this number
-    to: '+17344363982'// From a valid Twilio number
+    to: '+16137093672', // Cell phone number
+    from: '+17344363982', // Twilio number
   })
-  .then(message => console.log(message.sid));
+  .then((message) => console.log(message.sid));
